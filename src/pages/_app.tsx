e88816230c10d71d30c34b289e60056879c9b776
Hasham-dev <vakanihasham64@gmail.com>
2023-02-14 18:53:25 +0500

    layout updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,19 +3,23 @@ import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 
 import theme from 'theme'
+import Nav from 'components/Navbar/Index'
+import Footer from 'components/Footer/Index'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider resetCSS theme={theme}>
       <Head>
-        <title>NextJS with Chakra-ui Boilerplate</title>
+        <title>First Lines</title>
         <link rel="shortcut icon" href="/img/chakra-logo.png" />
         <link rel="apple-touch-icon" href="/img/chakra-logo.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="description" content="NextJS with Chakra-ui Boilerplate" />
       </Head>
+      <Nav />
 
       <Component {...pageProps} />
+      <Footer />
     </ChakraProvider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,52 +4,46 @@ import Container from 'components/Container'
 import Features from 'components/Features'
 import Testimonials from 'components/Testimonials/Index'
 import Pricing from 'components/Pricing/Index'
-import Footer from 'components/Footer/Index'
-import Nav from 'components/Navbar/Index'
 import { Box, Text, VStack } from '@chakra-ui/react'
 import { FAQSection } from 'components/FAQ/Index'
 
 const faqs: any[] = [
   {
-    q: "How many clients can I bring on?",
-    a: "You can bring on 3 clients with the Free plan. Upgrade to Pro for additional seats.",
+    q: 'How many clients can I bring on?',
+    a: 'You can bring on 3 clients with the Free plan. Upgrade to Pro for additional seats.'
   },
   {
-    q: "Can I connect it to my CRM?",
-    a: "Yes! We support Notion and PipeDrive currently.",
+    q: 'Can I connect it to my CRM?',
+    a: 'Yes! We support Notion and PipeDrive currently.'
   },
   {
-    q: "Do you support international payments?",
-    a: "Yes - payments can be made from and to any country.",
+    q: 'Do you support international payments?',
+    a: 'Yes - payments can be made from and to any country.'
   },
   {
-    q: "Who can I connect to for support?",
-    a: "Email me at sukh@saasbase.dev",
-  },
-];
-
+    q: 'Who can I connect to for support?',
+    a: 'Email me at sukh@saasbase.dev'
+  }
+]
 
 const Index = () => (
   <>
-      <Nav />
     <Container>
-
-    <Hero />
-    <Features />
-    <Testimonials />
+      <Hero />
+      <Features />
+      <Testimonials />
       <Pricing />
       <Container py={28} minW="container.md">
-          <Box w="full">
-            <VStack spacing={10} w="full">
-              <Text fontWeight={500} fontSize="2xl" align="center">
-                Frequently asked questions
-              </Text>
-              <FAQSection items={faqs} />
-            </VStack>
-          </Box>
-        </Container>
-  </Container>
-    <Footer />
+        <Box w="full">
+          <VStack spacing={10} w="full">
+            <Text fontWeight={500} fontSize="2xl" align="center">
+              Frequently asked questions
+            </Text>
+            <FAQSection items={faqs} />
+          </VStack>
+        </Box>
+      </Container>
+    </Container>
   </>
 )
 
