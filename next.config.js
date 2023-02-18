d5e1bf1215c0baa39374a4c16986b4a129b0cd8f
Hasham-dev <vakanihasham64@gmail.com>
2023-02-18 14:41:51 +0500

    updates

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
-const isProduction = process.env.NODE_ENV === 'production'
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+ 
+}
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: true
-  }
-})
+module.exports = nextConfig
